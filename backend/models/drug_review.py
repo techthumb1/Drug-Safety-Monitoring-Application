d4930c8e5d06e1056bed82fa7bf71475882a1dcb
Jason Robinson <robinsonjason761@gmail.com>
2023-08-20 05:47:52 -0500

    Established routes for connecting to database

---
+++ backend/models/drug_review.py
@@ -0,0 +1,10 @@
+# backend/models/drug_review.py
+
+from backend import db
+
+class DrugReview(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    drug_name = db.Column(db.String(128), nullable=False)
+    review_text = db.Column(db.Text, nullable=False)
+    adverse_event = db.Column(db.String(256))
+    reported_at = db.Column(db.DateTime, default=db.func.current_timestamp())
--- backend/routes/main_routes.py
+++ backend/routes/main_routes.py
@@ -8,11 +8,24 @@ main_routes = Blueprint('main_routes', __name__)
 @main_routes.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        drug_name = request.form.get('drug_name')
         review_text = request.form.get('review_text')
-        # Logic to save review and maybe predict side effects
+        
+        new_review = DrugReview(drug_name=drug_name, review_text=review_text)
+        db.session.add(new_review)
+        db.session.commit()
+        
         prediction = review_service.predict_severe_side_effects(review_text)
         return render_template('index.html', prediction=prediction)
+    
     return render_template('index.html')
+
+
+@main_routes.route('/all_reviews', methods=['GET'])
+def all_reviews():
+    reviews = DrugReview.query.all()
+    return render_template('all_reviews.html', reviews=reviews)
+
 
 # Additional configuration or initialization code
 
