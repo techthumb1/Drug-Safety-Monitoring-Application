799a6b78060516b6bf49095adce0a708b2faed3f
Jason Robinson <robinsonjason761@gmail.com>
2023-11-10 22:05:31 -0600

    Adding routes for data processing

--- backend/app.py
+++ backend/app.py
@@ -1,51 +1,46 @@
-# backend/app/app.py
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-###from flask import Flask
-###from flask_sqlalchemy import SQLAlchemy
-###from flask_migrate import Migrate
-###
-#### Initialize the Flask application
-###app = Flask(__name__)
-###
-#### Load configurations from environment or config file
-#### app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-#### app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-###
-###app = Flask(__name__)
-###app.config.from_object(DevelopmentConfig)
-###
-#### Initialize SQLAlchemy and Flask-Migrate
-###db = SQLAlchemy(app)
-###migrate = Migrate(app, db)
-###
-#### Importing routes
-###from .routes import main_routes
-###
-#### Registering routes with the app
-###app.register_blueprint(main_routes)
-###
-#### Models and services can be imported below or in their respective files
-###
-###if __name__ == "__main__":
-###    app.run(debug=True)
-###
-
-from flask import Flask
+# Import the 'db' object from a module called 'extensions'
+# It's assumed that 'extensions.py' exists and it contains an initialized 'SQLAlchemy' instance
 from extensions import db
 
-def create_app():
+# This import statement assumes you have a DevelopmentConfig class defined in a config.py file
+from config import DevelopmentConfig
+
+# Function to create the Flask application
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    
+    # Load configurations from the provided class
+    app.config.from_object(config_class)
     
     # Initialize extensions
     db.init_app(app)
+    Migrate(app, db)
+    
+    # Register blueprints
+    from .routes.main_routes import main_routes
+    app.register_blueprint(main_routes)
 
-    # Register blueprints here
-    from routes.main_routes import main
-    app.register_blueprint(main)
+    # Import models here if needed, after initializing the app context
+    # from .models import YourModel
+
+    # Define any routes here or in their respective blueprint modules
+    @app.route('/process-data', methods=['POST'])
+    def process_data():
+        try:
+            # Assuming process_openfda_data() is a function defined in the services module
+            from .services.review_service import process_openfda_data
+            process_openfda_data()
+            return jsonify({"status": "success", "message": "Data processing initiated."}), 200
+        except Exception as e:
+            return jsonify({"status": "error", "message": str(e)}), 500
     
     return app
 
+# Check if the run.py file is executing the app
 if __name__ == '__main__':
     app = create_app()
-    app.run()
+    app.run(debug=True)
--- backend/config.py
+++ backend/config.py
@@ -5,3 +5,33 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'postgresql://robinsonjason761@gmail.com:h@Tm7K!Mc9@localhost:5432/drugsafetydb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+import os
+
+class DevelopmentConfig:
+    # Enables debug mode to provide detailed error logs and auto-reload
+    DEBUG = True
+    
+    # The database URI that should be used for the connection
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'postgresql://robinsonjason761@gmail.com:h@Tm7K!Mc9@localhost:5432/drugsafetydb'
+    
+    # Track modifications of objects and emit signals. This requires extra memory and should be disabled if not needed.
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # Secret key for session management and CSRF protection, use a random value for production
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard_to_guess_string_for_development'
+    
+    # You can add more configuration options as needed
+    # ...
+
+# You may also define other config classes for production, testing, etc.
+class ProductionConfig:
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@localhost/prod_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'your_production_secret_key'
+    # Other production-specific settings
+
+# You can add more configuration classes if you have multiple environments
+
